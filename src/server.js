0f329138eace80baeeffa6435536da34e95dd022
kksree <kksree@gmail.com>
2019-11-06 11:33:12 +0530

    initilized version

--- src/server.js
+++ src/server.js
@@ -5,12 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ebasket'));
+app.use(express.static(__dirname + '/../dist/ebasket'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/ebasket/index.html'));
+res.sendFile(path.join(__dirname+'/../dist/ebasket/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080,()=> {
+    console.log("Listening in port 8080..");
+});
